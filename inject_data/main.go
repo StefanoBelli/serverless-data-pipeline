4c81f3d4c85cd485ef98a20386af0b56ddaae2ab
StefanoBelli <stefano9913@gmail.com>
2024-04-26 16:02:06 +0200

    Defining project structure

---
+++ inject_data/main.go
@@ -0,0 +1,9 @@
+package main
+
+const URL = "https://drive.usercontent.google.com/download?id=1mqkh5NOnXcPbMaDtlQwbqAohh2hmwD9A&" +
+	"export=download&authuser=0&confirm=t&uuid=e9f71f77-81ff-43a9-8a61-3b5c7e707aa1&" +
+	"at=APZUnTVXgI56OrhPdA_2E6QDCYca%3A1714139639065"
+
+func main() {
+
+}
