245659c01e005108732ca231f6ca0ba90459c8ec
StefanoBelli <stefano9913@gmail.com>
2024-05-09 12:52:12 +0200

    authorizer lambda code

--- lambdas/authorizer/main.go
+++ lambdas/authorizer/main.go
@@ -1,15 +1,83 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager/types"
 )
 
-type MyEvent struct {
-	Content string `json:"content"`
+func dflCtx() context.Context {
+	return context.TODO()
 }
 
-func handler(e MyEvent) (string, error) {
-	return e.Content, nil
+func newSecretsManagerService() (*secretsmanager.Client, error) {
+	awsConfig, err := config.LoadDefaultConfig(
+		dflCtx(),
+		config.WithRegion(os.Getenv("AWS_REGION")))
+	if err != nil {
+		return nil, err
+	}
+
+	return secretsmanager.NewFromConfig(awsConfig), nil
+}
+
+func getSecretValue() (string, error) {
+	var i1 int32 = 1
+
+	smSvc, err := newSecretsManagerService()
+	if err != nil {
+		return "", err
+	}
+
+	lsi := secretsmanager.ListSecretsInput{
+		SortOrder:  types.SortOrderTypeDesc,
+		MaxResults: &i1,
+		Filters: []types.Filter{
+			{
+				Key:    types.FilterNameStringTypeName,
+				Values: []string{"DataPipelineAuthKey"},
+			},
+		},
+	}
+	lso, err := smSvc.ListSecrets(dflCtx(), &lsi)
+	if err != nil {
+		return "", err
+	}
+
+	if len(lso.SecretList) == 0 {
+		return "", errors.New("SecretList is empty")
+	}
+
+	gsvi := secretsmanager.GetSecretValueInput{
+		SecretId: lso.SecretList[0].ARN,
+	}
+	gsvo, err := smSvc.GetSecretValue(dflCtx(), &gsvi)
+	if err != nil {
+		return "", err
+	}
+
+	return string(gsvo.SecretBinary), nil
+}
+
+type AuthorizationResponse struct {
+	IsAuthorized bool `json:"isAuthorized"`
+}
+
+func handler(event events.APIGatewayProxyRequest) (AuthorizationResponse, error) {
+	realKey, err := getSecretValue()
+	if err != nil {
+		return AuthorizationResponse{IsAuthorized: false}, err
+	}
+
+	return AuthorizationResponse{
+		IsAuthorized: realKey == event.Headers["Authorization"],
+	}, nil
 }
 
 func main() {
